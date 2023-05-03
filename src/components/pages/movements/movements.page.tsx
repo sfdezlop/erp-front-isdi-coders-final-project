dd7a211e5aed2cb6353801a4965d08bf34ee6d90
sfernandez <sfdezlop@gmail.com>
2023-05-03 21:45:59 +0200

    Order the fields

--- src/components/pages/movements/movements.page.tsx
+++ src/components/pages/movements/movements.page.tsx
@@ -82,23 +82,26 @@ export default function MovementsPage() {
         <div className="productMovementsPage__container">
           <div className="productMovementsPage__fieldContainer">
             <div className="productMovementsPage__field">Date</div>
-            <div className="productMovementsPage__field">Brand</div>
             <div className="productMovementsPage__field">SKU</div>
-            <div className="productMovementsPage__field">Batch</div>{" "}
+            <div className="productMovementsPage__field">Brand</div>
+            <div className="productMovementsPage__field">Image</div>
+            <div className="productMovementsPage__field">Stock</div>
+            <div className="productMovementsPage__field">Batch</div>
             <div className="productMovementsPage__field">Store</div>
             <div className="productMovementsPage__field">Units</div>
             <div className="productMovementsPage__field">Type</div>
-            <div className="productMovementsPage__field">Type ID</div>
-            <div className="productMovementsPage__field">Cost/unit</div>
-            <div className="productMovementsPage__field">Price/unit</div>
-            <div className="productMovementsPage__field">Stock</div>
-            <div className="productMovementsPage__field">Image</div>
+            <div className="productMovementsPage__field">TypeID</div>
+            <div className="productMovementsPage__field">Cost/ut</div>
+            <div className="productMovementsPage__field">Price/ut</div>
             <div className="productMovementsPage__field">Delete</div>
           </div>
           <div className="productMovementsPage__dataContainer">
             {filteredGalleryData.map((item) => (
               <li className="productMovementsPage__dataRow" key={item.id}>
                 <div className="productMovementsPage__data">{item.date}</div>
+                <div className="productMovementsPage__data">
+                  {item.productSku}
+                </div>
                 <div className="productMovementsPage__data">
                   <ProductKeyValue
                     urlExtraPathId={
@@ -107,9 +110,16 @@ export default function MovementsPage() {
                     }
                   ></ProductKeyValue>
                 </div>
-
                 <div className="productMovementsPage__data">
-                  {item.productSku}
+                  <ProductKeyValue
+                    urlExtraPathId={
+                      "microservices/inputkey-sku-outputkey-image/inputvalue-" +
+                      item.productSku
+                    }
+                  ></ProductKeyValue>
+                </div>{" "}
+                <div className="productMovementsPage__data">
+                  <ProductStock sku={item.productSku}></ProductStock>
                 </div>
                 <div className="productMovementsPage__data">{item.batch}</div>
                 <div className="productMovementsPage__data">{item.store}</div>
@@ -121,17 +131,6 @@ export default function MovementsPage() {
                 </div>
                 <div className="productMovementsPage__data">
                   {item.pricePerUnit}
-                </div>
-                <div className="productMovementsPage__data">
-                  <ProductStock sku={item.productSku}></ProductStock>
-                </div>
-                <div className="productMovementsPage__data">
-                  <ProductKeyValue
-                    urlExtraPathId={
-                      "microservices/inputkey-sku-outputkey-image/inputvalue-" +
-                      item.productSku
-                    }
-                  ></ProductKeyValue>
                 </div>
                 <div className="productMovementsPage__data productMovementsPage__delete">
                   <button
