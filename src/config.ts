3f4b6fb806a7ca27c0ed093eb12fe625c211fd10
sfernandez <sfdezlop@gmail.com>
2023-05-19 20:38:58 +0200

    Change to render.com server

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const url_local = "http://localhost:4500";
 const url_render = "https://erp-back-isdi-coders-final-project.onrender.com";
 
-const localConnection = true;
+const localConnection = false;
 
 export const url_def = localConnection ? url_local : url_render;
 
